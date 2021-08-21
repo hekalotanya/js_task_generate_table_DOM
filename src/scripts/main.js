a2f51b923e067d7bc74dd1998d8053f74e4b0fe5
hekalotanya <hekalot@gmail.com>
2021-08-21 13:05:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,40 @@
 'use strict';
 
 const people = require('./lib/people');
-
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
+
+const board = document.querySelector('.dashboard');
+
+for (const i of people) {
+  const row = document.createElement('tr');
+  const name = document.createElement('th');
+
+  name.textContent = i.name;
+  row.append(name);
+
+  const gender = document.createElement('th');
+
+  gender.textContent = i.sex;
+  row.append(gender);
+
+  const born = document.createElement('th');
+
+  born.textContent = i.born;
+  row.append(born);
+
+  const died = document.createElement('th');
+
+  died.textContent = i.died;
+  row.append(died);
+
+  const age = document.createElement('th');
+
+  age.textContent = i.died - i.born;
+  row.append(age);
+
+  const century = document.createElement('th');
+
+  century.textContent = Math.ceil(i.died / 100);
+  row.append(century);
+  board.append(row);
+}
